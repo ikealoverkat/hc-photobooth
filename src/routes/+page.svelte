ccd0ba90491f0c41b14c29199e549a3341adc2ad
kat <katwang7749@gmail.com>
2026-08-04 19:06:00 -0400

    add video preview & photo-taking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let video: HTMLVideoElement;
+	let canvas: HTMLCanvasElement;
+
+	let stream: MediaStream;
+
+	onMount(async () => {
+		stream = await navigator.mediaDevices.getUserMedia({ video: true });
+		video.srcObject = stream;
+        await video.play();
+	});
+
+    function takePhoto() {
+        const context = canvas.getContext('2d');
+
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        context?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+        const image = canvas.toDataURL('image/png');
+
+        console.log(image);
+    }
+</script>
+
+<h1>i said hey whats up hello</h1>
+
+<video bind:this={video} class="scale-x-[-1]"></video>
+
+<button onclick={takePhoto}>take photo</button>
+
+<canvas bind:this={canvas} class="scale-x-[-1]"></canvas>
