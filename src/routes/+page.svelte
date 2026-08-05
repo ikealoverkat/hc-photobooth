5308dc7214c23b2291f3c1318a6ad6b139f89a8d
kat <katwang7749@gmail.com>
2026-08-04 21:33:30 -0400

    add placeholder photoframes section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,12 @@
 		selected: boolean;
 	};
 
+	type Frame = {
+		src: string;
+		name: string;
+		selected: boolean;
+	};
+
 	let photos: Photo[] = $state([]);
 
 	let isTakingPhotos = $state(false);
@@ -20,6 +26,9 @@
 	let countdown: number = $state(0);
 
 	let selector: HTMLDivElement;
+
+	let frames: Frame[] = [];
+	// import this once i actually have frames
 
 	onMount(async () => {
 		stream = await navigator.mediaDevices.getUserMedia({ video: true });
@@ -80,14 +89,24 @@
 			});
 		}
 	}
+
+	function toggleFrameSelect(frame: Frame) {
+		const selectedCount = photos.filter((f) => f.selected).length;
+		if (!frame.selected && selectedCount >= 1) {
+			return;
+		}
+        frame.selected = !frame.selected;
+	}
 </script>
 
 <main class="dots-bg flex flex-col items-center justify-center gap-4 p-4">
 	<!-- photo taking part -->
-	<div class="hover:scale-101 duration-200 flex h-screen flex-col items-center justify-center">
-		<div class="flex flex-col items-center justify-center gap-4 p-24 bg-red/10 backdrop-blur-[2.5px] outline-2 outline-red/35 shadow-md shadow-red/35">
-			<div class="text-center mb-8">
-				<h1 class="text-dark-red font-phantom text-4xl font-bold">hack club photobooth!</h1>
+	<div class="flex h-screen flex-col items-center justify-center duration-200 hover:scale-101">
+		<div
+			class="flex flex-col items-center justify-center gap-4 bg-red/10 p-24 shadow-md shadow-red/35 outline-2 outline-red/35 backdrop-blur-[2.5px]"
+		>
+			<div class="mb-8 text-center">
+				<h1 class="font-phantom text-4xl font-bold text-dark-red">hack club photobooth!</h1>
 				<p class="font-phantom text-2xl text-dark-red/75">
 					it's just a normal photobooth, but all the frames are hack club themed :p
 				</p>
@@ -95,18 +114,31 @@
 
 			<div class="flex flex-col items-center justify-center gap-2">
 				{#if countdown !== 0}
-					<div class="text-4xl font-phantom text-dark-red">{countdown}</div>
+					<div class="font-phantom text-4xl text-dark-red">{countdown}</div>
 				{/if}
-				<video bind:this={video} autoplay playsinline muted class="scale-x-[-1] outline-2 outline-dark-red"></video>
+				<video
+					bind:this={video}
+					autoplay
+					playsinline
+					muted
+					class="scale-x-[-1] outline-2 outline-dark-red"
+				></video>
 			</div>
 
 			<button onclick={photobooth} disabled={isTakingPhotos}
-				><img src={buttonSource} alt="click to take photos" width="75px" height="75px" class="duration-200 opacity-50 hover:opacity-75 hover:scale-108 button-shadow" /></button
+				><img
+					src={buttonSource}
+					alt="click to take photos"
+					width="75px"
+					height="75px"
+					class="button-shadow opacity-50 duration-200 hover:scale-108 hover:opacity-75"
+				/></button
 			>
 		</div>
 	</div>
 	<canvas bind:this={canvas} class="hidden"> </canvas>
 
+	<!-- select photos -->
 	<div bind:this={selector} class="hidden h-screen flex-col items-center justify-center gap-4">
 		<h1 class="m-4 font-phantom text-4xl font-bold text-dark-red">choose four pictures to keep</h1>
 		<div
@@ -128,4 +160,25 @@
 		</div>
 		<button class="red-button bright-red-shadow">next!</button>
 	</div>
+
+	<!-- choose frame -->
+	<div class="flex flex-col h-screen gap-4">
+		<h1 class="text-4xl font-phantom text-dark-red font-bold">choose a photo frame!</h1>
+		<div class="flex flex-row overflow-x-scroll -px-16">
+			{#each frames as frame}
+				<!-- svelte-ignore a11y_click_events_have_key_events -->
+				<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+				<img
+					src={frame.src}
+					alt={frame.name}
+					class={frame.selected
+						? 'outline-4 outline-red duration-200 hover:scale-102 hover:z-100 active:scale-101'
+						: 'outline duration-200 hover:scale-103'}
+					onclick={() => toggleFrameSelect(frame)}
+				/>
+			{/each}
+		</div>
+	</div>
+	<!-- "print" photostrip -->
+	<div></div>
 </main>
