d77a9c20d0fe467ffcef0d6f527c2095d87ed8d6
kat <katwang7749@gmail.com>
2026-08-07 00:20:41 -0400

    add sfx to gallery page & change photo copying logic so all popups dont show at the same time

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -5,14 +5,32 @@
 
 	let gallery: string[] = $state([]);
 
-    let copied = $state(false);
+	let copiedImage = $state<string | null>(null);
+
+	let hoverButton: HTMLAudioElement;
+	let clickButton: HTMLAudioElement;
 
 	onMount(() => {
+		hoverButton = new Audio('/sounds/hover.mp3');
+		clickButton = new Audio('/sounds/click1.mp3');
+
 		const saved = localStorage.getItem('gallery');
 		if (saved) {
 			gallery = JSON.parse(saved);
 		}
 	});
+
+	function playHoverButton() {
+		if (!hoverButton) return;
+		hoverButton.currentTime = 0;
+		hoverButton.play();
+	}
+
+	function playClickButton() {
+		if (!clickButton) return;
+		clickButton.currentTime = 0;
+		clickButton.play();
+	}
 
 	function downloadPhotoStrip(image: string) {
 		const link = document.createElement('a');
@@ -32,10 +50,12 @@
 				})
 			]);
 			console.log('image copied to clipboard');
-            copied = true;
-            setTimeout(() => {
-                copied = false
-            }, 2000)
+			copiedImage = image;
+			setTimeout(() => {
+				if (copiedImage === image) {
+					copiedImage = null;
+				}
+			}, 2000);
 		} catch (error: any) {
 			console.error('failed to copy image: ', error.name, error.message);
 		}
@@ -54,31 +74,46 @@
 	</p>
 	<div class="flex flex-col gap-4">
 		{#if gallery.length == 0}
-			<h1 class="text-xl text-dark-red/50 italic">huh, looks like your gallery is empty. go <a href="/" class="text-red/65 font-bold underline hover:decoration-wavy">take some photos!</a></h1>
+			<h1 class="text-xl text-dark-red/50 italic">
+				huh, looks like your gallery is empty. go <a
+					href="/"
+					class="font-bold text-red/65 underline hover:decoration-wavy">take some photos!</a
+				>
+			</h1>
 		{/if}
-		<div class="flex flex-row flex-wrap gap-4 mb-8">
+		<div class="mb-8 flex flex-row flex-wrap gap-4">
 			{#each gallery as photo}
 				<div class="flex flex-col items-center justify-center gap-6">
-                    {#if copied}
-                        <div class="absolute p-4 bg-dark-red/75 rounded-2xl text-lg -mt-4 text-white font-phantom popup-fadeout">copied to clipboard!</div>	
-                    {/if}
-                    <img src={photo} alt="saved photostrip" class="w-2/3 outline outline-dark-red" />
+					{#if copiedImage === photo}
+						<div
+							class="popup-fadeout absolute -mt-4 rounded-2xl bg-dark-red/75 p-4 font-phantom text-lg text-white"
+						>
+							copied to clipboard!
+						</div>
+					{/if}
+					<img src={photo} alt="saved photostrip" class="w-2/3 outline outline-dark-red" />
 					<div class="flex flex-row gap-4">
 						<button
 							class="countdown-button"
+							onmouseenter={playHoverButton}
 							onclick={() => {
+								playClickButton();
 								copyPhotoStrip(photo);
 							}}><Copy color="#57040e" size={15} strokeWidth={2} /></button
 						>
 						<button
 							class="countdown-button"
+							onmouseenter={playHoverButton}
 							onclick={() => {
+								playClickButton();
 								downloadPhotoStrip(photo);
 							}}><ArrowDownToLine color="#57040e" size={15} strokeWidth={2} /></button
 						>
 						<button
 							class="countdown-button-selected"
+							onmouseenter={playHoverButton}
 							onclick={() => {
+								playClickButton();
 								deleteFromStorage(photo);
 							}}><Trash2 color="#57040e" size={15} strokeWidth={2} /></button
 						>
